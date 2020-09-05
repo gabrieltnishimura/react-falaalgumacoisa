d068fa396f88f06dbe957a78ed0f0d640097e433
Gabriel Nishimura <gabriel.t.nishimura@accenture.com>
2020-09-05 19:25:58 -0300

    feat(material-ui): also started form basic structure

--- src/basic-data/BasicDataPage.tsx
+++ src/basic-data/BasicDataPage.tsx
@@ -1,3 +1,9 @@
+import FormControl from '@material-ui/core/FormControl';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import Select from '@material-ui/core/Select';
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import WhitePageWrapper from '../shell/WhitePageWrapper';
@@ -5,12 +11,25 @@ import BasicDataService from './BasicDataService';
 import HeaderFormStep from './HeaderFormStep';
 import { useInput } from './useInput';
 
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    formControl: {
+      minWidth: 120,
+    },
+    selectEmpty: {
+      marginTop: theme.spacing(2),
+    },
+  }),
+);
+
 function BasicDataPage(props: any) {
-  const { value: initials, bind: bindInitials } = useInput('');
+  const { value: initials, bind: bindFirstName } = useInput('');
   const { value: sex, bind: bindSex } = useInput<'M' | 'F'>('M');
-  const { value: age, bind: bindAge } = useInput<number>(0);
+  const { value: age, bind: bindAgeInterval } = useInput<number>(0);
   const { value: origin, bind: bindOrigin } = useInput('');
   const navigate = useNavigate();
+  const classes = useStyles();
 
   const handleSubmit = (evt: any) => {
     evt.preventDefault();
@@ -20,54 +39,30 @@ function BasicDataPage(props: any) {
       navigate('/gravar', { replace: true });
     });
   }
-
-  /**
-   * Based on TIMIT, for each speaker:
-   *  - the ID (speaker's initials), 
-   *  - Sex (male or female), 
-   *  - DR (dialect region), 
-   *  - Use (train or test), 
-   *  - RecDate (recording date),
-   *  - BirthDate, 
-   *  - Ht (height), 
-   *  - Race, 
-   *  - Edu (education level) 
-   *  - and optional comments listing interesting speaker attributes or abnormalities.
-   */
   return (
     <WhitePageWrapper>
       <div>
         <HeaderFormStep title="Dados Básicos" step="1/2"></HeaderFormStep>
         <div>
-          <form onSubmit={handleSubmit}>
-            <div>
-              <label>
-                Iniciais:
-        <input type="text" maxLength={2} {...bindInitials} />
-              </label>
-            </div>
-            <div>
-              <label>
-                Sexo:
-        <select {...bindSex}>
-                  <option value="M">Masculino</option>
-                  <option value="F">Feminino</option>
-                </select>
-              </label>
-            </div>
-            <div>
-              <label>
-                Ano de nascimento:
-        <input type="number" minLength={4} maxLength={4} {...bindAge} />
-              </label>
-            </div>
-            <div>
-              <label>
-                Região de origem:
-        <input type="text" {...bindOrigin} />
-              </label>
-            </div>
-            <input type="submit" value="Enviar" />
+          <form noValidate autoComplete="off" onSubmit={handleSubmit}>
+            <TextField fullWidth label="Primeiro Nome" {...bindFirstName} />
+            <FormControl fullWidth className={classes.formControl}>
+              <InputLabel id="demo-simple-select-label">Faixa etária</InputLabel>
+              <Select
+                labelId="demo-simple-select-label"
+                id="demo-simple-select"
+                {...bindAgeInterval}
+              >
+                <MenuItem value={1}>até 25</MenuItem>
+                <MenuItem value={2}>26-35</MenuItem>
+                <MenuItem value={3}>36-45</MenuItem>
+                <MenuItem value={4}>46-55</MenuItem>
+                <MenuItem value={5}>56-65</MenuItem>
+                <MenuItem value={6}>66-75</MenuItem>
+                <MenuItem value={7}>mais que 75</MenuItem>
+              </Select>
+            </FormControl>
+            <TextField fullWidth label="Standard" />
           </form>
         </div>
       </div>
