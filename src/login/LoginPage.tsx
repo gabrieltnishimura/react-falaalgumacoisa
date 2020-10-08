c75d65d3e31e764b76455f3b46b6c80b1d22c864
Gabriel Nishimura <gabriel.t.nishimura@accenture.com>
2020-10-08 16:52:14 -0300

    styles(login): fields and buttons

--- src/login/LoginPage.tsx
+++ src/login/LoginPage.tsx
@@ -1,18 +1,31 @@
+import { createStyles, makeStyles, Theme, ThemeProvider } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import { authenticationService } from '../authentication/AuthenticationService';
 import { useInput } from '../basic-data/useInput';
+import RectangularButton from '../shared/RectangularButton';
 import Header from '../shell/Header';
 import LinkItem from '../shell/LinkItem';
+import theme from '../shell/theme';
 import WhitePageWrapper from '../shell/WhitePageWrapper';
 import FacebookLoginButton from '../splash/FacebookLoginButton';
 import GoogleLoginButton from '../splash/GoogleLoginButton';
 import styles from './LoginPage.module.css';
 
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      '& .MuiFormControl-fullWidth': {
+        marginBottom: theme.spacing(2),
+      },
+    },
+  }),
+);
 
 function LoginPage() {
   const navigate = useNavigate();
+  const classes = useStyles();
   const { value: username, bind: bindUsername } = useInput('');
   const { value: password, bind: bindPassword } = useInput('');
 
@@ -32,24 +45,28 @@ function LoginPage() {
     authenticationService.loginWithUsername(username, password);
   }
 
+  const handleSubmit = (evt: any) => {
+    evt.preventDefault();
+  }
+
   return (
-    <div>
+    <ThemeProvider theme={theme}>
       <Header />
       <WhitePageWrapper>
         <div className={styles.enterTextWrapper}>
           <h1 className={styles.enterText}>Entrar</h1>
         </div>
-        <div className={styles.username}>
-          <TextField fullWidth label="Usuário" {...bindUsername} />
-        </div>
-        <div className={styles.password}>
-          <TextField fullWidth label="Senha" {...bindPassword} />
-        </div>
-        <div className={styles.continueButtonWrapper}>
-          <div className={styles.continueButton} onClick={loginWithUsernameFn}>
-            <span className={styles.continueButtonText}>Continuar</span>
+        <form noValidate onSubmit={handleSubmit} className={`${classes.root} ${styles.fullHeight}`}>
+          <div className={styles.username}>
+            <TextField fullWidth label="Usuário" name="username" {...bindUsername} />
           </div>
-        </div>
+          <div className={styles.password}>
+            <TextField type="password" fullWidth label="Senha" name="password" {...bindPassword} />
+          </div>
+          <div className={styles.continueButtonWrapper}>
+            <RectangularButton title="Continuar" primary submit onClick={loginWithUsernameFn} ></RectangularButton>
+          </div>
+        </form>
         <div>
           <hr className={styles.separator} />
         </div>
@@ -66,7 +83,7 @@ function LoginPage() {
           <GoogleLoginButton click={googleClick}></GoogleLoginButton>
         </div>
       </WhitePageWrapper>
-    </div >
+    </ThemeProvider>
   );
 }
 
--- src/splash/FacebookLoginButton.tsx
+++ src/splash/FacebookLoginButton.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
-import styles from './FacebookLoginButton.module.css';
+import facebookStyles from './FacebookLoginButton.module.css';
+import styles from './SocialMediaButtons.module.css';
 
 function FacebookLoginButton(props: { click: () => void }) {
   return (
-    <div className={styles.button} onClick={props.click}>
+    <div className={`${styles.button} ${facebookStyles.button}`} onClick={props.click}>
       <div>
         <div className={styles.iconWrapper}>
-          <svg className={styles.facebookIcon} viewBox="0 0 408.788 408.788">
+          <svg className={`${styles.icon} ${facebookStyles.icon}`} viewBox="0 0 408.788 408.788">
             <path d="M353.701,0H55.087C24.665,0,0.002,24.662,0.002,55.085v298.616c0,30.423,24.662,55.085,55.085,55.085
 	h147.275l0.251-146.078h-37.951c-4.932,0-8.935-3.988-8.954-8.92l-0.182-47.087c-0.019-4.959,3.996-8.989,8.955-8.989h37.882
 	v-45.498c0-52.8,32.247-81.55,79.348-81.55h38.65c4.945,0,8.955,4.009,8.955,8.955v39.704c0,4.944-4.007,8.952-8.95,8.955
