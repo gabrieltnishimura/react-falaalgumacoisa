7b68882ca07b11c8cfdd5b3013da28e3d2df84d9
Gabriel Nishimura <gabriel.t.nishimura@accenture.com>
2020-10-16 00:34:58 -0300

    fix(homepage-animation): removed setTimeout, using transitions and keyframes

--- src/home/HomePage.tsx
+++ src/home/HomePage.tsx
@@ -7,29 +7,19 @@ import Header from '../shell/Header';
 import HomeContent from './HomeContent';
 import styles from './HomePage.module.css';
 import SplashContent from './SplashContent';
-let once = false;
 
 function HomePage() {
   const { setLoading } = (React.useContext(LoaderContext) as LoaderContextInterface);
   const navigate = useNavigate();
   const [animate, setAnimate] = useState(false);
-  const [skipAnimations, setSkipAnimations] = useState(false);
   const imageLoaded = useProgressiveImage('/splash-cover.png')
-  const navigationTimeout = 800;
   const [randomizedTheme, setRandomizedTheme] = useState<string>('');
 
   useEffect(() => {
     // wait for image and theme to load
     if (imageLoaded && randomizedTheme) {
       setLoading(false); // each page has to implement its own stop loading logic
-      if (once) {
-        setSkipAnimations(true);
-        return;
-      }
-      setTimeout(() => {
-        setAnimate(true);
-        once = true;
-      }, navigationTimeout);
+      setAnimate(true);
     }
   }, [imageLoaded, randomizedTheme, setLoading]);
 
@@ -50,11 +40,8 @@ function HomePage() {
   }
 
   return (
-    <div className={`${skipAnimations ? styles.skip : animate ? styles.animation : ''}`}>
+    <>
       <div className={styles.banner}>
-        <div className={styles.header}>
-          <Header link={{ title: 'Entrar', onClick: redirectLoginFn }} />
-        </div>
         {imageLoaded ?
           <img className={styles.bannerImage} src={imageLoaded} alt='Banner'></img> :
           null}
@@ -66,10 +53,15 @@ function HomePage() {
           </div>
         </div>
       </div>
-      <div className={styles.content}>
-        <HomeContent redirectRecordingFn={redirectRecordingFn} />
-      </div>
-    </div>
+      {animate ? <div>
+        <div className={styles.header}>
+          <Header link={{ title: 'Entrar', onClick: redirectLoginFn }} />
+        </div>
+        <div className={styles.content}>
+          <HomeContent redirectRecordingFn={redirectRecordingFn} />
+        </div>
+      </div> : null}
+    </>
   );
 }
 
