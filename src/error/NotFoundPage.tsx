4a8f75f99d2688906c587a69304480166a2d1139
Gabriel Nishimura <gabriel.t.nishimura@accenture.com>
2020-10-24 11:59:52 -0300

    feat(404): fallback route

---
+++ src/error/NotFoundPage.tsx
@@ -0,0 +1,37 @@
+import React, { useEffect } from 'react';
+import RectangularButton from '../shared/buttons/RectangularButton';
+import AttentionIcon from '../shared/icons/AttentionIcon';
+import { LoaderContext, LoaderContextInterface } from '../shared/loader/LoaderContext';
+import ErrorHeader from '../shell/ErrorHeader';
+import WhitePageWrapper from '../shell/WhitePageWrapper';
+import styles from './ErrorPage.module.css';
+
+function NotFoundPage() {
+  const { setLoading } = (React.useContext(LoaderContext) as LoaderContextInterface);
+
+  useEffect(() => {
+    setLoading(false);
+  });
+
+  const back = () => {
+    window.location.href = "/";
+  }
+
+  return (
+    <>
+      <ErrorHeader />
+      <WhitePageWrapper>
+        <div className={styles.content}>
+          <AttentionIcon></AttentionIcon>
+          <h1 className={styles.title}>Página não encontrada</h1>
+          <RectangularButton title="Voltar para home" onClick={back} primary></RectangularButton>
+        </div>
+      </WhitePageWrapper>
+      <div className={styles.footer}>
+        <img src="/splash-cover.jpg" alt="cover" />
+      </div>
+    </>
+  );
+}
+
+export default NotFoundPage;
